1c78742e08770f8d5ac56c52a1c85fd497db811d
Raudhil <raudhilfirdausn@gmail.com>
2023-10-29 22:57:53 +0700

    Added Looping Customer to Kasir.java

--- Kasir.java
+++ Kasir.java
@@ -24,7 +24,6 @@ public class Kasir {
                 payment,
                 change;
 
-
         // Input username and password to login.
         while (true) {
             System.out.println("Please login first.");
