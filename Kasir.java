c60356aaa048161811af79c25f17ace99535227c
Raudhil <raudhilfirdausn@gmail.com>
2023-10-29 22:55:37 +0700

    Added Looping Customer to Kasir.java

--- Kasir.java
+++ Kasir.java
@@ -1,35 +1,45 @@
 package Java_Restaurant_Cashier;
-
 import java.util.Scanner;
 public class Kasir {
     public static void main(String[] args) {
 
         // Variables declaration.
         Scanner input = new Scanner(System.in);
+        String[] friedRice = {"Fried Rice Pax", "Rp. 22000", "22000"};
+        String[] friedChicken = {"Fried Rice Pax", "Rp. 23000", "23000"};
+        String[] hamburger = {"Fried Rice Pax", "Rp. 24000", "24000"};
 
-        String username, TrueUsername, TruePassword, password, horizonline, OrderMore;
-        int table = 2, id_payment_type, menu1, menu2, menu3, id_menu = 1;
-        double total_price = 0.0, payment, change;
-
-        TrueUsername = "kelompok6";
-        TruePassword = "kelompok6";
-        horizonline = "=======================================";
+        String username,
+                trueUsername = "kelompok6",
+                truePassword = "kelompok6",
+                password,
+                orderMore;
+        int table = 3,
+                id_payment_type,
+                id_menu = 1,
+                customer = 1,
+                amount;
+        double total_price = 0.0,
+                todays_income = 0.0,
+                payment,
+                change;
 
         // Input username and password to login.
         while (true) {
             System.out.println("Please login first.");
-            System.out.println(horizonline);
+            System.out.println("=======================================");
             System.out.print("Username  : ");
-            username = input.nextLine();
+            username = input.next();
             System.out.print("Password  : ");
-            password = input.nextLine();
+            password = input.next();
             System.out.println("=======================================");
             // Check if the username and password is correct.
-            if (username.equals(TrueUsername) && password.equals(TruePassword)){
+            if (username.equals(trueUsername) && password.equals(truePassword)) {
                 System.out.println("Login success.");
                 System.out.println("=======================================");
                 break;
-            }if (!username.equals(TrueUsername) && !password.equals(TruePassword))  {
+            }
+            if (!username.equals(trueUsername) && !password.equals(truePassword)) {
                 System.out.println("Wrong username and password");
                 System.out.println("=======================================");
             } else {
@@ -38,9 +48,12 @@ public class Kasir {
             }
         }
         // Check if there's table available.
+        for (int i = 0 ; i < customer ; i++){
+            System.out.println("Customer " + customer);
         System.out.println("Checking if there's table available...");
         if (table != 0) {
-            System.out.println("Table available.");
+            table--;
+            System.out.println(table + " Table available.");
             System.out.println("=======================================");
 
             // Choose menu.
@@ -48,7 +61,6 @@ public class Kasir {
                 System.out.println("0. Pay\n1. Fried Rice Pax \n2. Fried Chicken Pax \n3. Hamburger Pax");
                 System.out.print("Please choose your menu : ");
                 id_menu = input.nextInt();
-                input.nextLine();
                 switch (id_menu) {
                     case 0:
                         System.out.println("=======================================");
@@ -58,26 +70,29 @@ public class Kasir {
                         continue;
                     case 1:
                         System.out.println("=======================================");
-                        System.out.println("Fried Rice Pax = Rp.22000");
-                        menu1 = 22000;
-                        total_price = total_price + menu1;
-                        System.out.println("Total Price : " + total_price);
+                        System.out.println(friedRice[0] + " = " + friedRice[1]);
+                        System.out.print("Total amount: ");
+                        amount = input.nextInt();
+                        total_price = total_price + Integer.parseInt(friedRice[2]) * amount;
+                        System.out.println("Total Price : Rp. " + total_price);
                         System.out.println("=======================================");
                         break;
                     case 2:
                         System.out.println("=======================================");
-                        System.out.println("Fried Chicken Pax = Rp.23000");
-                        menu2 = 23000;
-                        total_price = total_price + menu2;
-                        System.out.println("Total Price : " + total_price);
+                        System.out.println(friedChicken[0] + " = " + friedChicken[1]);
+                        System.out.print("Total amount: ");
+                        amount = input.nextInt();
+                        total_price = total_price + Integer.parseInt(friedChicken[2]) * amount;
+                        System.out.println("Total Price : Rp. " + total_price);
                         System.out.println("=======================================");
                         break;
                     case 3:
                         System.out.println("=======================================");
-                        System.out.println("Hamburger Pax = Rp.24000");
-                        menu3 = 24000;
-                        total_price = total_price + menu3;
-                        System.out.println("Total Price : " + total_price);
+                        System.out.println(hamburger[0] + " = " + hamburger[1]);
+                        System.out.print("Total amount: ");
+                        amount = input.nextInt();
+                        total_price = total_price + Integer.parseInt(hamburger[2]) * amount;
+                        System.out.println("Total Price : Rp. " + total_price);
                         System.out.println("=======================================");
                         break;
                     default:
@@ -86,24 +101,23 @@ public class Kasir {
                         System.out.println("=======================================");
                 }
 
-
-                //Choose to order more or not.
+                // Choose to order more or not.
                 while (true) {
                     System.out.println("Do you want to order more? (y/n)");
                     System.out.print("Your answer : ");
-                    OrderMore = input.nextLine();
+                    orderMore = input.next();
 
-                    if (OrderMore.equalsIgnoreCase("n")) {
+                    if (orderMore.equalsIgnoreCase("n")) {
                         System.out.println("Please choose payment type.");
                         break;
-                    }else if (OrderMore.equalsIgnoreCase("y")) {
+                    } else if (orderMore.equalsIgnoreCase("y")) {
                         System.out.println("Please choose your menu : ");
                         break;
-                    }else {
+                    } else {
                         System.out.println("Please answer (y/n)");
                     }
                 }
-                if (OrderMore.equalsIgnoreCase("n")) {
+                if (orderMore.equalsIgnoreCase("n")) {
                     break;
                 }
             }
@@ -113,8 +127,9 @@ public class Kasir {
             id_payment_type = input.nextInt();
 
             // Cash payment type.
-            if (id_payment_type == 1 ){
+            if (id_payment_type == 1) {
                 System.out.println("=======================================");
+                System.out.println("Total price           : " + total_price);
                 System.out.print("Input payment nominal : ");
                 payment = input.nextInt();
                 change = payment - total_price;
@@ -126,6 +141,8 @@ public class Kasir {
                     System.out.println("Printing receipt...");
                     System.out.println("Thanks for the purchase!");
                     System.out.println("=======================================");
+                    todays_income += total_price;
+                    total_price = 0.0;
                 } else {
                     System.out.println("=======================================");
                     System.out.println("Please input the correct nominal.");
@@ -150,7 +167,11 @@ public class Kasir {
         } else {
             System.out.println("There's no table available.");
             System.out.println("=======================================");
+            break;
         }
-
+            todays_income+= total_price;
+            customer++;
+       }
+        System.out.println("Today's income is : Rp. " + todays_income);
     }
 }
